52342115515f32cfe640592580af2bdea6f12155
hundag20 <hundaguluma@gmail.com>
2024-03-08 16:56:47 +0300

    finalizing wizard

--- index.js
+++ index.js
@@ -36,9 +36,49 @@ exports.ussdController = async (req, res) => {
   }
   else if (/^2\*/.test(text) && !text.substring(2).includes("*")) {
     // This is the first request. Note how we start the response with CON
-    response = `CON Welcome to Mind Matters. Choose the service you want to get
-            3. schedule for consultaion`;
-  } else if (text) {
+    response = `CON Welcome to Mind Matters. Choose a service
+
+    3. schedule for consultation
+    4. read tips`
+            ;
+  } else if (/^3\*/.test(text) && !text.substring(2).includes("*")) {
+    // This is the first request. Note how we start the response with CON
+    response = `CON What do you need consultation on?
+
+    5. Lonliness
+    6. Discuss symptoms
+    7. Ask mental health related questions
+    `
+            ;
+  }
+  else if (/^[567]\*.*/.test(text) && !text.substring(2).includes("*")) {
+    // This is the first request. Note how we start the response with CON
+    response = `CON Choose date, experty and rate$ that works for you?
+
+    8. March 21 2024 8 am - 9 am | Dr. Bizuneh Abate (M) | hourly rate: 500 ETB
+    9. March 26 2024 3 pm - 4 pm | Dr. Marta Solomon (F)| hourly rate: 700 ETB
+    10. March 18 2024 10 am - 11 am | Dr. Meron Girma (F)| hourly rate: 350 ETB
+    `
+            ;
+  }
+  else if (/^[891][0-9]*\*./.test(text) && !text.substring(2 + text.match(/^\d+/)[0].length).includes("*")) {
+    const result = await africasTalking.SMS.send({
+        to: phoneNumber,
+        //message is random 6 digit number to simulate OTP
+        message: "Deposit amount on selected session @ CBE to account: 1000272819665 OR @ telebirr to ID: 4357. NOTE: include this reference number in comments/reference section of deposit (REF: 1039047284). After doing that wait for our verification of payment. Thank you.",
+        from: "MindMatters",
+      });
+
+    // This is the first request. Note how we start the response with CON
+    response = `END Follow these steps to finalize booking of session:
+    
+    Step 1: make full payment based on the instructions sent you via SMS. 
+    Step 2: Wait for confirmation of payment
+    Step 3: Be ready for receiving a call for the telephone session on the selected date
+    `
+            ;
+  }
+   else if (text) {
     response = `END invalid input, please refer the menu`;
   }
 
