b2a194cbf363aff2cbbe82490a2f33e04238544a
hundag20 <hundaguluma@gmail.com>
2024-03-08 12:22:08 +0300

    test africastalking api

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 //get any request and return with 'hi'
 const express = require('express');
 const app = express();
+const ussdController = require('./index.js');
 
 app.get('/', (req, res) => {
     res.send('hi');
 });
+app.post(
+    "/ussd",
+    (req, res, next) => {
+      next();
+    },
+    ussdController
+  );
 
 app.listen(3000, () => {
     console.log('Listening on port 3000');
---
+++ index.js
@@ -0,0 +1,40 @@
+
+export async function ussdHandler(req, res) {
+  const { sessionId, serviceCode, phoneNumber, text } = req.body;
+  console.log("###########", req.body);
+  // myLogger.info(req.body);
+
+  let response = "";
+
+  if (text == "") {
+    // This is the first request. Note how we start the response with CON
+    response = `CON 1. Login`;
+  } else if (text == "1") {
+    // This is the first request. Note how we start the response with CON
+    response = `CON Enter license id to login`;
+  } else if (/^1\*/.test(text) && !text.substring(2).includes("*")) {
+    //REMINDER: validate pwd entered doesn't contain */ that is is alphanumeric
+    // This is the first request. Note how we start the response with CON
+            response = `CON Welcome to Traffic Management system
+            2. get payment link
+            3. set password
+            4. get information`;
+  } else if (text) {
+    response = `END invalid input, please refer the menu`;
+  }
+
+  res.set("Content-Type: text/plain");
+  res.send(response);
+}
+
+export function ussdHandler2(req, res) {
+  console.log("ussd is posted");
+}
+
+//NOTE no login required because phone_num is already the validator, login would be redundant
+//TODO
+//--get link to payment --- by rec of evaluators
+//reset password(new pwd nd confirm)
+//news (put the option and decide at admin if we're doing it or not)
+
+//NOTE: ussd auth: enter license, if license and phone num match cont.
